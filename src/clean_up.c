a9a108c1471cee0f913794c1e23b62a50ebae052
Balineisenbart <anton.stoiber@gmail.com>
2025-06-08 12:39:08 +0200

    still protect mtx_init & all joins and creates

--- src/clean_up.c
+++ src/clean_up.c
@@ -5,27 +5,35 @@
 //need to modularize .> clean mutex fork & print_lock separately
 //wa print_lock locked?? -> separate check??
 
-int clean_up(t_symposium *symposium)
+bool clean_up(t_symposium *symposium)
 {
     t_fork *f = symposium->fork;
     t_fork *e_f = f + symposium->n_philo;
     t_philo *p = symposium->philo;
     t_philo *e_p = p + symposium->n_philo;
+    bool error_status;
+
+    error_status = false;
 
     if (pthread_join(symposium->death_thread, NULL))
     {
         printf("pthread_join death_thread fialed. Dig for error code for more info\n");
-        return(1);
-
+        error_status = true;
     }
     if (symposium->n_meals != -1)
-        pthread_join(symposium->finish_thread, NULL);
+    {
+        if (pthread_join(symposium->finish_thread, NULL))
+        {
+            printf("pthread_join finish_thread fialed. Dig for error code for more info\n");
+            error_status = true;
+        }
+    }
     while (p < e_p)
     {
         if (pthread_join(p->thread_id, NULL))
         {
             printf("pthread_join thread_id fialed. Dig for error code for more info\n");
-            return(1);
+             error_status = true;
         }
         p++;
     }
@@ -39,7 +47,7 @@ int clean_up(t_symposium *symposium)
                 if (pthread_mutex_destroy(&p->meal_lock))
                 {
                     printf ("Mutex destroy failed on meal_lock\n");
-                    return (1);
+                    error_status = true;
                 }
             }
             if (p->full_mtx_init)
@@ -47,7 +55,7 @@ int clean_up(t_symposium *symposium)
                 if(pthread_mutex_destroy(&p->full_lock))
                 {
                     printf ("Mutex destroy failed on full_lock\n");
-                    return (1);
+                    error_status = true;
                 }
             }
             p++;
@@ -64,7 +72,7 @@ int clean_up(t_symposium *symposium)
                 if (pthread_mutex_destroy(&f->fork))
                 {
                     printf ("Mutex destroy failed on fork :: in clean_up\n");
-                    return (1);
+                    error_status = true;
                 }
             }
             f++;
@@ -77,7 +85,7 @@ int clean_up(t_symposium *symposium)
         if (pthread_mutex_destroy(&symposium->print_lock))
         {
             printf ("Mutex destroy failed on print_lock\n");
-            return (1);
+            error_status = true;
         }
     }
     if (symposium->finish_mtx_init)
@@ -85,8 +93,8 @@ int clean_up(t_symposium *symposium)
         if (pthread_mutex_destroy(&symposium->finish_lock))
         {
             printf ("Mutex destroy failed on finish_lock\n");
-            return (1);
+            error_status = true;
         }
     }
-    return (0);
+    return (error_status); //still needs to be modularized to adhere to line count
 }
